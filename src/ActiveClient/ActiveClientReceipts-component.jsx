3a98e58273a3e0b54d691505224a5d20b4431147
RobJ <robjjohnsenwork@gmail.com>
2022-10-24 11:00:16 +0700

    Home Page logo added

--- src/ActiveClient/ActiveClientReceipts-component.jsx
+++ src/ActiveClient/ActiveClientReceipts-component.jsx
@@ -1,25 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { useOutletContext, Link } from "react-router-dom";
 import { useGlobalContext } from "../Context-Reducer/Context";
 //
-const dummyData = [
-  // {
-  //   date: "2022-10-22",
-  //   id: "qweqw",
-  //   amount: "322",
-  //   sessions: 5,
-  // },
-  // {
-  //   date: "2022-09-15",
-  //   id: "gqwe",
-  //   amount: "550",
-  //   sessions: 10,
-  // },
-];
 
 const ActiveClientReceipts = () => {
-  // const client = useOutletContext();
-  //   console.log(client);
+  //
   const { focused } = useGlobalContext();
   const [orderedReceipts, setOrderedReceipts] = useState([]);
   //
@@ -41,7 +25,6 @@ const ActiveClientReceipts = () => {
   //
   return (
     <div className="bg-blue-100 w-full h-full overflow-auto flex flex-col gap-2">
-      {/*<div className="p-2 gap-3 flex flex-col border border-black">*/}
       <div className="bg-purple-300 h-10 w-full pt-2 mt-2 text-xl underline p-2 flex text-center ">
         <h2 className="w-1/3 underline">Date</h2>
         <h2 className="w-1/3 underline">Amount</h2>
@@ -66,32 +49,8 @@ const ActiveClientReceipts = () => {
             })}
         </div>
       </div>
-      {/*</div>*/}
     </div>
   );
 };
 
 export default ActiveClientReceipts;
-
-//receipts: { debits: [], credits: [] },
-// const debitTemplate = {
-//   date: "",
-//   id: "",
-//   amount: "",
-//   sessions: 0,
-// };
-//
-// <div className="flex flex-col gap-1">
-//   {filteredClients.map((client) => {
-//     return (
-//       <Link
-//         key={client.id}
-//         to={`/clientList/${client.id}`}
-//         className="w-full h-10 bg-green-200 flex justify-around items-center"
-//       >
-//         <h2>{client.name}</h2>
-//         <p>{client.age}</p>
-//       </Link>
-//     );
-//   })}
-// </div>;
--- src/Home/Home-component.jsx
+++ src/Home/Home-component.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import logo from "../Images/mai-method-resized.png";
 //
 const Home = () => {
   return (
     <div className="bg-white w-full h-full">
-      <h2>Hey, welcome to Mai Method, lets explore</h2>
+      <div className="flex flex-col h-full w-full bg-gray-500">
+        <div className=" p-10 mt-5">
+          <img
+            src={logo}
+            alt="Mai Method Logo"
+            className="w-full bg-white rounded-2xl"
+          />
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Home;
+//
+//<img src={logo} alt="Mai Method Logo" className="h-full " />
